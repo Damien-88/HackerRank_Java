bfac3af6b59c3f4f3f0636dcd7d21cb0c6b5dc54
Nikolaj <wochnikkai@gmail.com>
2025-03-22 13:27:29 -0700

    Added try for the scanner and change data type comparison code.

--- IntToString.java
+++ IntToString.java
@@ -5,26 +5,23 @@ public class IntToString {
     
     public static void main(String[] args) {
         // Create a Scanner object to read input from the user
-        Scanner sc = new Scanner(System.in);
-        
-        // Read an integer input from the user
-        int number = sc.nextInt();
-        
-        // Convert the integer to a string
-        String str = Integer.toString(number);
-        
-        // Check if the convertion is of string type
-        if (str.getClass().getName() == "java.lang.String") {
-            // If true, print "Good job"
-            System.out.println("Good job");
-        } 
-        else {
-            // If false, print "Wrong answer"
-            System.out.println("Wrong answer");
+        try (Scanner sc = new Scanner(System.in)) {
+            // Read an integer input from the user
+            int number = sc.nextInt();
+            
+            // Convert the integer to a string
+            String str = Integer.toString(number);
+            
+            // Check if the convertion is of string type
+            if (str.getClass().getName().equals("java.lang.String")) {
+                // If true, print "Good job"
+                System.out.println("Good job");
+            } 
+            else {
+                // If false, print "Wrong answer"
+                System.out.println("Wrong answer");
+            }
         }
-        
-        // Close the Scanner object to release resources
-        sc.close();
     }
 
 }
